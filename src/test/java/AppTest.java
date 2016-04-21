8539902cc20deb9899723c61136cf4d36ca046f1
Ashley Maceli and DJ Roychoudhury <dj@>
2016-04-21 14:49:36 -0700

    Add user interface passing tests

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,32 @@
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+    return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("What are you allergic to?");
+  }
+
+  @Test
+  public void allergies() {
+    goTo("http://localhost:4567/");
+    fill("#number").with("2");
+    submit(".btn");
+    assertThat(pageSource()).contains("[peanuts]");
+  }
+}
